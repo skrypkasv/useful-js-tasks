3241e01db893afad018f96607bc06df7b3ec854f
skrypkasv <skrypkasv@gmail.com>
2020-08-31 13:15:39 +0300

    Fixes my_functions.js

--- my_functions.js
+++ my_functions.js
@@ -12,6 +12,39 @@
 //
 //
 //
+//
+//
+//
+//
+//
+// Функция возведет в квадрат каждую
+// цыфру из числа(num) и вернет новое
+// число
+// function squareDigits(num) {
+//   let stringResult = '';
+//   let result;
+
+//   for (let item of String(num).split('')) {
+//     item = Math.pow(Number(item), 2);
+//     stringResult += item;
+//     result = Number(stringResult);
+//   }
+//   console.log(result);
+// }
+// squareDigits(3223);
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
 // //Функция изменит регистр каждой буквы в строке/слове
 // const changeCase = function (string) {
 //   let invertedString = '';
@@ -24,6 +57,11 @@
 //   return invertedString;
 // };
 // console.log(changeCase('We aRe The chaMpiOns'));
+//
+//
+//
+//
+//
 //
 //
 //
